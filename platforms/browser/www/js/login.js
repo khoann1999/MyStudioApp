1d0d3ea7fe36a82b0c514d16f5b157d8d8a9c4ec
khoaNN <khoann1999@gmail.com>
2020-07-03 21:43:31 +0700

    no message

---
+++ platforms/browser/www/js/login.js
@@ -0,0 +1,34 @@
+var url = "https://localhost:44368/api/Accounts"
+$(document).ready(function () {
+    $("#login").click(function (e) { 
+        e.preventDefault();
+        var username = $("#txtUsername").val().trim();
+        var password = $("#txtPassword").val().trim();
+        var data = {"username":username, "password":password, "role": "aass"};
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: url,
+            data: JSON.stringify(data),
+            success: function (response) {
+                if( response == 200){
+                    window.location = "home.html";
+                }else{
+                    msg = "Invalid username and password";
+                }
+                $("#message").html(msg);
+            }
+        });
+    });
+    // $("#login").click(function (e) { 
+    //     $.ajax({
+    //         type: "GET",
+    //         url: url,
+    //         success: function (response) {
+                
+    //         }
+    //     });
+        
+    // });
+});
